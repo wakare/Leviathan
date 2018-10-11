71193f3911d6518a244d79e2c88c1f585563b4fd
wangjie <wangjie@launcamedical.com>
2018-10-11 13:26:29 +0800

    Add vertexTypeMask

--- RenderWrapper/GLObject.h
+++ RenderWrapper/GLObject.h
@@ -5,11 +5,19 @@
 class GLObject
 {
 public:
-	GLObject(GLuint primType, GLuint vertexCount) :m_VAO(0), m_VBO(0), m_primitiveType(primType),m_vertexCount(vertexCount) {}
+	enum VertexAttributeType
+	{
+		VERTEX_ATTRIBUTE_XYZ = 0x1,		// Coord XYZ
+		VERTEX_ATTRIBUTE_RGBA = 0x2,	// Color RGBAs
+		VERTEX_ATTRIBUTE_NXYZ = 0x4,	// Normal XYZ
+	};
+
+	GLObject(GLuint primType, GLuint vertexCount, GLint vertexMask) :m_VAO(0), m_VBO(0), m_primitiveType(primType),m_vertexCount(vertexCount),m_vertexAttributeMask(vertexMask) {}
 	GLuint GetVAO() { return m_VAO; }
 	GLuint GetVBO() { return m_VBO; }
 	GLuint GetPrimType() { return m_primitiveType; }
 	GLuint GetVertexCount() { return m_vertexCount; }
+	GLuint GetVertexMask() { return m_vertexAttributeMask; }
 
 	virtual bool Init() = 0;
 
@@ -18,4 +26,5 @@ protected:
 	GLuint m_VBO;
 	GLuint m_primitiveType;
 	GLuint m_vertexCount;
+	GLint m_vertexAttributeMask;
 };
