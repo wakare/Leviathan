7c59d1cbdc993585198196baa7fed3183949f3e0
wakare <wakare_no_kaze@outlook.com>
2019-11-19 22:55:27 +0800

    update project vc version to 142

--- 3rdparty/assimp/build/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Git/Leviathan/3rdparty/assimp/assimp-4.1.0/contrib/zlib
-# Build directory: D:/Git/Leviathan/3rdparty/assimp/build/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
